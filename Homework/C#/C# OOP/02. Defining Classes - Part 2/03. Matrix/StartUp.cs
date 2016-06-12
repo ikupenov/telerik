bec965b3c61bc1d6c9870195367730326e0b9119
Iliyan Kupenov <dmxyyyy@gmail.com>
2016-06-12 21:26:07 +0300

    Update

--- Homework/C#/C# OOP/02. Defining Classes - Part 2/03. Matrix/StartUp.cs
+++ Homework/C#/C# OOP/02. Defining Classes - Part 2/03. Matrix/StartUp.cs
@@ -6,70 +6,10 @@
     {
         static void Main()
         {
-            Matrix<int> test1 = new Matrix<int>(3, 2);
-            Matrix<int> test2 = new Matrix<int>(2, 2);
-
-            test1[0, 0] = 4;
-            test1[0, 1] = 8;
-            test1[1, 0] = 0;
-            test1[1, 1] = 2;
-            test1[2, 0] = 1;
-            test1[2, 1] = 6;
-
-            Console.WriteLine(new string('=', 50));
-            Console.WriteLine("1st Matrix");
-            Console.WriteLine(new string('_', 15));
-            Console.WriteLine(test1);
-            Console.WriteLine();
-
-            test2[0, 0] = 5;
-            test2[0, 1] = 2;
-            test2[1, 0] = 9;
-            test2[1, 1] = 4;
-
-            Console.WriteLine("2nd Matrix");
-            Console.WriteLine(new string('_', 15));
-            Console.WriteLine(test2);
-            Console.WriteLine();
-
-            Console.WriteLine("Product of FIRST and SECOND Matrices");
-            Console.WriteLine(new string('_', 15));
-            Console.WriteLine(test1 * test2);
-            Console.WriteLine(new string('=', 50));
-            Console.WriteLine();
-
-            Matrix<int> test3 = new Matrix<int>(2, 2);
-            Matrix<int> test4 = new Matrix<int>(2, 2);
-
-            test3[0, 0] = 10;
-            test3[0, 1] = 28;
-            test3[1, 0] = 47;
-            test3[1, 1] = 86;
-
-            Console.WriteLine("3rd Matrix");
-            Console.WriteLine(new string('_', 15));
-            Console.WriteLine(test3);
-            Console.WriteLine();
-
-            test4[0, 0] = 42;
-            test4[0, 1] = 13;
-            test4[1, 0] = 11;
-            test4[1, 1] = 38;
-
-            Console.WriteLine("4th Matrix");
-            Console.WriteLine(new string('_', 15));
-            Console.WriteLine(test4);
-            Console.WriteLine();
-
-            Console.WriteLine("Sum of THIRD and FOURTH Matrices");
-            Console.WriteLine(new string('_', 15));
-            Console.WriteLine(test3 + test4);
-            Console.WriteLine();
-
-            Console.WriteLine("Difference of THIRD and FOURTH Matrices");
-            Console.WriteLine(new string('_', 15));
-            Console.WriteLine(test3 - test4);
-            Console.WriteLine(new string('=', 50));
+            Test.Multiplication();
+            Test.Addition();
+            Test.Subtraction();
+            Console.ReadKey();
         }
     }
 }
---
+++ Homework/C#/C# OOP/02. Defining Classes - Part 2/03. Matrix/Test.cs
@@ -0,0 +1,125 @@
+﻿namespace NumberMatrix
+{
+    using System;
+
+    public class Test
+    {
+        public static void Multiplication ()
+        {
+            Matrix<int> test1 = new Matrix<int>(3, 2);
+            Matrix<int> test2 = new Matrix<int>(2, 2);
+
+            Random rnd = new Random();
+
+            for (int i = 0; i < test1.GetLength(0); i++)
+            {
+                for (int j = 0; j < test1.GetLength(1); j++)
+                {
+                    test1[i, j] = rnd.Next(-100, 100);
+                }
+            }
+
+            Console.WriteLine(new string('=', 50));
+            Console.WriteLine("1st Matrix");
+            Console.WriteLine(new string('_', 20));
+            Console.WriteLine(test1);
+
+            for (int i = 0; i < test2.GetLength(0); i++)
+            {
+                for (int j = 0; j < test2.GetLength(1); j++)
+                {
+                    test2[i, j] = rnd.Next(-100, 100);
+                }
+            }
+
+            Console.WriteLine("\r\n2nd Matrix");
+            Console.WriteLine(new string('_', 20));
+            Console.WriteLine(test2);
+
+            Console.WriteLine("\r\nProduct of Matrices");
+            Console.WriteLine(new string('_', 20));
+            Console.WriteLine(test1 * test2);
+            Console.WriteLine(new string('-', 50));
+        }
+
+        public static void Addition()
+        {
+            Console.WriteLine();
+            Console.WriteLine(new string('=', 50));
+
+            Matrix<int> test1 = new Matrix<int>(2, 2);
+            Matrix<int> test2 = new Matrix<int>(2, 2);
+
+            Random rnd = new Random();
+
+            for (int i = 0; i < test1.GetLength(0); i++)
+            {
+                for (int j = 0; j < test1.GetLength(1); j++)
+                {
+                    test1[i, j] = rnd.Next(-100, 100);
+                }
+            }
+
+            Console.WriteLine("1st Matrix");
+            Console.WriteLine(new string('_', 20));
+            Console.WriteLine(test1);
+
+            for (int i = 0; i < test2.GetLength(0); i++)
+            {
+                for (int j = 0; j < test2.GetLength(1); j++)
+                {
+                    test2[i, j] = rnd.Next(-100, 100);
+                }
+            }
+
+            Console.WriteLine("\r\n2nd Matrix");
+            Console.WriteLine(new string('_', 20));
+            Console.WriteLine(test2);
+
+            Console.WriteLine("\r\nSum of Matrices");
+            Console.WriteLine(new string('_', 20));
+            Console.WriteLine(test1 + test2);
+            Console.WriteLine(new string('-', 50));
+        }
+
+        public static void Subtraction()
+        {
+            Console.WriteLine();
+            Console.WriteLine(new string('=', 50));
+
+            Matrix<int> test1 = new Matrix<int>(2, 2);
+            Matrix<int> test2 = new Matrix<int>(2, 2);
+
+            Random rnd = new Random();
+
+            for (int i = 0; i < test1.GetLength(0); i++)
+            {
+                for (int j = 0; j < test1.GetLength(1); j++)
+                {
+                    test1[i, j] = rnd.Next(-100, 100);
+                }
+            }
+
+            Console.WriteLine("1st Matrix");
+            Console.WriteLine(new string('_', 20));
+            Console.WriteLine(test1);
+
+            for (int i = 0; i < test2.GetLength(0); i++)
+            {
+                for (int j = 0; j < test2.GetLength(1); j++)
+                {
+                    test2[i, j] = rnd.Next(-100, 100);
+                }
+            }
+
+            Console.WriteLine("\r\n2nd Matrix");
+            Console.WriteLine(new string('_', 20));
+            Console.WriteLine(test2);
+
+            Console.WriteLine("\r\nDifference of Matrices");
+            Console.WriteLine(new string('_', 20));
+            Console.WriteLine(test1 - test2);
+            Console.WriteLine(new string('-', 50));
+        }
+    }
+}
