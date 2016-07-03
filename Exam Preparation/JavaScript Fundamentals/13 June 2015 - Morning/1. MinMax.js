067fa4a16c6b2b4f674bfb9a0a2177875b2adf04
Iliyan Kupenov <dmxyyyy@gmail.com>
2016-07-03 17:20:31 +0300

    Update

--- Exam Preparation/JavaScript Fundamentals/13 June 2015 - Morning/1. MinMax.js
+++ Exam Preparation/JavaScript Fundamentals/13 June 2015 - Morning/1. MinMax.js
@@ -2,23 +2,11 @@ function solve(params) {
     var n = parseInt(params[0]),
         k = parseInt(params[1]),
         numbers = params[2].split(' ').map(Number),
-        result = [],        
-        min, max;
+        result = [];
 
     for (var i = 0; i <= n - k; i += 1) {
-        min = numbers[i];
-        max = numbers[i];
-        
-        for (var j = i; j < k + i; j += 1) {
-            if (numbers[j] < min) {
-                min = numbers[j];
-            }
-            if (numbers[j] > max) {
-                max = numbers[j];
-            }
-        }
-
-        result.push(min + max);
+        result.push(Math.min.apply(null, numbers.slice(i, i + k)) +
+            Math.max.apply(null, numbers.slice(i, i + k)));
     }
 
     console.log(result.join(','));
