487d6cf6345fae2ee1efe7b41952bf2a5a86bd87
Iliyan Kupenov <dmxyyyy@gmail.com>
2016-07-29 22:06:05 +0300

    Second Homework Upload

---
+++ Homework/C#/C# Unit Testing/02. Test-Driven Development/Test-Driven Development/Poker/Hand.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Poker
+{
+    public class Hand : IHand
+    {
+        public IList<ICard> Cards { get; private set; }
+
+        public Hand(IList<ICard> cards)
+        {
+            this.Cards = cards;
+        }
+
+        public override string ToString()
+        {
+            if (this.Cards == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            var builder = new StringBuilder();
+
+            foreach (var card in this.Cards)
+            {
+                if (card != null)
+                {
+                    builder.AppendLine(card.ToString());
+                }
+            }
+
+            return builder.ToString().Trim();
+        }
+    }
+}
---
+++ Homework/C#/C# Unit Testing/02. Test-Driven Development/Test-Driven Development/Poker/PokerHandsChecker.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Linq;
+
+namespace Poker
+{
+    public class PokerHandsChecker : IPokerHandsChecker
+    {
+        public bool IsValidHand(IHand hand)
+        {
+            if (hand == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            if (hand.Cards.Count != 5)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            for (int i = 0; i < hand.Cards.Count; i++)
+            {
+                for (int j = i + 1; j < hand.Cards.Count; j++)
+                {
+                    if (hand.Cards[i].Face == hand.Cards[j].Face &&
+                        hand.Cards[i].Suit == hand.Cards[j].Suit)
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+
+        public bool IsStraightFlush(IHand hand)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsFourOfAKind(IHand hand)
+        {
+            IsValidHand(hand);
+
+            var isFourOfAKind = hand.Cards.GroupBy(x => x.Face)
+                                          .Count(x => x.Count() == 4);
+
+            return isFourOfAKind == 1 ? true : false;
+        }
+
+        public bool IsFullHouse(IHand hand)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsFlush(IHand hand)
+        {
+            IsValidHand(hand);
+
+            var isFlush = hand.Cards.GroupBy(x => x.Suit)
+                                    .Count(x => x.Count() == 5);
+
+            return isFlush == 1 ? true : false;
+        }
+
+        public bool IsStraight(IHand hand)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsThreeOfAKind(IHand hand)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsTwoPair(IHand hand)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsOnePair(IHand hand)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsHighCard(IHand hand)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int CompareHands(IHand firstHand, IHand secondHand)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
