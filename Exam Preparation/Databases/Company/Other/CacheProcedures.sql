1f182a146c4f6e42918dd3ac4f9118e12ff88541
Iliyan Kupenov <dmxyyyy@gmail.com>
2016-11-05 19:09:53 +0200

    adding cache procedures

--- Exam Preparation/Databases/Company/Other/CacheProcedures.sql
+++ Exam Preparation/Databases/Company/Other/CacheProcedures.sql
@@ -8,39 +8,38 @@ GO
 
 CREATE PROC usp_CreateQ3CacheTable
 AS
-	CREATE TABLE Q3CacheTable
-	(
-		Employee NVARCHAR(100),
-		Department NVARCHAR(50),
-		Project NVARCHAR(50),
-		StartDate DATETIME,
-		EndDate DATETIME,
-		ReportsCount INT
+	CREATE TABLE Q3CacheTable (
+		Employee NVARCHAR(100)
+		,Department NVARCHAR(50)
+		,Project NVARCHAR(50)
+		,StartDate DATETIME
+		,EndDate DATETIME
+		,ReportsCount INT
 	);
-GO 
+GO
 
 CREATE PROC usp_UpdateQ3CacheTable
 AS
 	DELETE FROM Q3CacheTable
 	INSERT INTO Q3CacheTable
-		SELECT
-			CONCAT(e.FirstName, ' ', e.LastName) AS [Employee]
-			,d.Name AS [Department]
-			,p.Name AS [Project]
-			,p.StartDate AS [StartDate]
-			,p.EndDate AS [EndDate]
-			,(SELECT
-					COUNT(*)
-				FROM Reports r
-				WHERE r.Time BETWEEN p.StartDate AND p.EndDate)
-			AS [ReportsCount]
-		FROM Employees e
-		JOIN Departments d
-			ON d.Id = e.DepartmentId
-		JOIN Employees_Projects ep
-			ON e.Id = ep.EmployeeId
-		JOIN Projects p
-			ON p.Id = ep.ProjectId
+			SELECT
+				CONCAT(e.FirstName, ' ', e.LastName) AS [Employee]
+				,d.Name AS [Department]
+				,p.Name AS [Project]
+				,p.StartDate AS [StartDate]
+				,p.EndDate AS [EndDate]
+				,(SELECT
+						COUNT(*)
+					FROM Reports r
+					WHERE r.Time BETWEEN p.StartDate AND p.EndDate)
+				AS [ReportsCount]
+			FROM Employees e
+			JOIN Departments d
+				ON d.Id = e.DepartmentId
+			JOIN Employees_Projects ep
+				ON e.Id = ep.EmployeeId
+			JOIN Projects p
+				ON p.Id = ep.ProjectId
 GO
 
 EXEC usp_CreateQ3CacheTable
